12308a8afbd78cd7dcb0dda43b86a3a3af5a97e4
TedoHarisC <tedoharischandra29@gmail.com>
2022-05-30 08:42:06 +0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/submission_dicoding_tedo_hc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.submission_dicoding_tedo_hc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kGreenColor = const Color(0xff24C690);
+Color kBlackColor = const Color(0xff000000);
+Color kGreyColor = const Color(0xff89807A);
+Color kCyanColor = const Color(0xff24C6C6);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.nunito(
+  color: kGreenColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.nunito(
+  color: kGreyColor,
+);
+
+TextStyle cyanTextStyle = GoogleFonts.nunito(
+  color: kCyanColor,
+);
